d2ef0d383fd4bff13908f37e392b5e5706a15d90
Kelsey Tripp <ktripp@circleup.com>
2016-10-10 14:48:20 -0700

    Add git completion

--- setup/setup.sh
+++ setup/setup.sh
@@ -19,5 +19,6 @@ launchctl load /usr/local/opt/mysql/homebrew.mxcl.mysql.plist
 mysql_tzinfo_to_sql /usr/share/zoneinfo | sed -e "s/Local time zone must be set--see zic manual page/local/" | mysql -u root mysql
 
 # set up bash_profile
+cp git-completion.bash ~/.git-completion.bash
 cp bash_profile ~/.bash_profile
 source ~/.bash_profile
