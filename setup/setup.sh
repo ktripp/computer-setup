cc10386e2ad5a72a827212c54240c6100196aa5f
Kelsey Tripp <ktripp@circleup.com>
2016-10-10 14:29:54 -0700

    Add bash_profile

---
+++ setup/setup.sh
@@ -0,0 +1,23 @@
+# install pip
+sudo easy_install pip
+
+# install brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# install git-aware-prompt
+mkdir ~/.bash
+cd ~/.bash
+git clone git://github.com/jimeh/git-aware-prompt.git
+
+# install virtualenv
+sudo pip install virtualenvwrapper
+
+# install mysql
+brew install mysql
+ln -sfv /usr/local/opt/mysql/*.plist
+launchctl load /usr/local/opt/mysql/homebrew.mxcl.mysql.plist 
+mysql_tzinfo_to_sql /usr/share/zoneinfo | sed -e "s/Local time zone must be set--see zic manual page/local/" | mysql -u root mysql
+
+# set up bash_profile
+cp bash_profile ~/.bash_profile
+source ~/.bash_profile
